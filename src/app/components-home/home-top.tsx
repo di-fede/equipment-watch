6d4dfddcaf74d56a64f5d0f4cd269ff3291abcde
Richard DiFede <difede462@gmail.com>
2026-04-21 14:14:14 -0400

    initial commit

---
+++ src/app/components-home/home-top.tsx
@@ -0,0 +1,21 @@
+export default function HomeTop() {
+    return (
+        <div className="top">
+            <div className="top__box">
+                <div className="top__location-container">
+                    Location: <div className="top__location">Delray Beach</div>
+                </div>
+                <div className="top__left">
+                    <div className="top__product-type">
+                        <span>Treadmill</span>
+                    </div>
+                    <div className="top__product-model">The ass kicker</div>
+                </div>
+                <div className="top__right">
+                    <div>No.</div>
+                    <div className="top__machineNumber">2</div>
+                </div>
+            </div>
+        </div>
+    );
+}
