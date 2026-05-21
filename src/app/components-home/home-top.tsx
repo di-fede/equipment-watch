e20ddd706690b8bab45178f214aa8414aec464d1
Richard DiFede <difede462@gmail.com>
2026-05-21 16:20:26 -0400

    fix logo

--- src/app/components-home/home-top.tsx
+++ src/app/components-home/home-top.tsx
@@ -1,20 +1,28 @@
+import { Roboto } from "next/font/google";
+
+const roboto = Roboto({
+    style: ["normal"],
+    subsets: ["latin"],
+    weight: ["300"],
+});
 export default function HomeTop() {
     return (
         <div className="top">
-            <div className="top__box">
-                <div className="top__location-container">
-                    Location: <div className="top__location">Delray Beach</div>
+            <div className={`tracker__title ${roboto.className}`}>
+                Equipment Tracker
+            </div>
+            <div className="top__location-container">
+                Location: <div className="top__location">Delray Beach</div>
+            </div>
+            <div className="top__equipId">
+                <div className="top__product-type">
+                    <span>Treadmill</span>
                 </div>
-                <div className="top__left">
-                    <div className="top__product-type">
-                        <span>Treadmill</span>
-                    </div>
-                    <div className="top__product-model">The ass kicker</div>
-                </div>
-                <div className="top__right">
-                    <div>No.</div>
-                    <div className="top__machineNumber">2</div>
-                </div>
+                <div className="top__product-model">The ass kicker</div>
+            </div>
+            <div className="top__right">
+                <div>No.</div>
+                <div className="top__machineNumber">2</div>
             </div>
         </div>
     );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
 import FormTreadmill from "./components-home/form-treadmill";
 import HomeTop from "./components-home/home-top";
+import LogoBox from "./components-home/logo-box";
 import Nav from "./components-home/nav";
 
 export default function Home() {
     return (
-        <>
+        <div className="main">
+            <LogoBox />
             <HomeTop />
             <FormTreadmill />
             <Nav />
-        </>
+        </div>
     );
 }
