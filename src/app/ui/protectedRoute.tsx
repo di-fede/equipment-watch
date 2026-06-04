ff2531dd557a31f25e9f005de51a33424a417ffd
Richard DiFede <difede462@gmail.com>
2026-06-04 12:41:11 -0400

    fix useLogin

--- src/app/ui/protectedRoute.tsx
+++ src/app/ui/protectedRoute.tsx
@@ -2,10 +2,11 @@
 
 import { ReactNode, useEffect } from "react";
 import { useUser } from "../authentication/useUser";
-import { useRouter } from "next/navigation";
+import { useRouter, usePathname } from "next/navigation";
 
 function ProtectedRoute({ children }: { children: ReactNode }) {
     const router = useRouter();
+    const pathname = usePathname();
 
     // 1. Load the authenticated user
     const { isLoading, isAuthenticated } = useUser();
@@ -13,12 +14,15 @@ function ProtectedRoute({ children }: { children: ReactNode }) {
     // 2. If no authenticated user, redirect to the /login
     useEffect(
         function () {
-            if (!isAuthenticated && !isLoading) {
+            if (!isAuthenticated && !isLoading && pathname !== "/login") {
                 router.push("/login");
             }
         },
-        [isAuthenticated, isLoading, router],
+        [isAuthenticated, isLoading, router, pathname],
     );
+
+    // If we are on the login page, just render the login page without protection
+    if (pathname === "/login") return children;
 
     // 3. Show loading spinner
     if (isLoading) return <h1 className="loading"> Loading</h1>;
